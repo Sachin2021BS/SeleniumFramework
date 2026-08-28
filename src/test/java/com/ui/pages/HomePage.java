4b3cac12f3aa237bbe8abf291e9f569f92f89d03
Sachin B S <sachinbs@Mac.lan>
2026-08-28 22:35:12 +0530

    Running test in Headless Mode

--- src/test/java/com/ui/pages/HomePage.java
+++ src/test/java/com/ui/pages/HomePage.java
@@ -15,8 +15,8 @@ public final class HomePage extends BrowserUtility {
     private static final By SIGN_IN_LINK_LOCATOR=By.xpath("//a[@class='login']");
 
 
-    public HomePage(Browser browserName) {
-        super(browserName); // To call the parent constructor we use super key word
+    public HomePage(Browser browserName,boolean isHeadless) {
+        super(browserName,isHeadless); // To call the parent constructor we use super key word
         //goToWebsite(readProperty(QA, "URL")); // This is for Property File
         goToWebsite(Jsonutility.readJsonFile(QA).getUrl()); // This is for JSON File
         maximizeWindow();
